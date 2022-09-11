208f4828b15fb97dac8b1a7e59ae71c1e4953a52
pdcourses <freshitpt@gmail.com>
2022-09-11 13:56:21 +0300

    simple server

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const fs = require("fs");
-const util = require("util");
-// вывести содержимое js файлов по текущей папке
-const readFile = util.promisify(fs.readFileSync);
-const contentDir = fs.readdirSync(".");
+const http = require('http');
+const server = http.createServer(requestListener);
+server.listen(3000);
 
-contentDir
-    .filter((f) => f.endsWith(".js"))
-    .forEach(f => readFile(f, {encoding: 'utf-8'})
-                .then(data => console.dir(data))
-                .catch(err => console.log(err))
-            );
-
-
-
+//req, res
+let count = 0;
+function requestListener(request, response){
+    console.log('It is my first server!',(++count));
+    response.end( `Hello client!!! msg # ${count}`);
+}
--- math.js
+++
@@ -1,4 +0,0 @@
-const sum = (a,b) => a+b;
-const sub = (a,b) => a-b;
-const mult = (a,b) => a*b;
-const div = (a,b) => a/b;
