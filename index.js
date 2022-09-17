843fb5b781b9db06f7c71bfbdd6f2be34afbd3f8
pdcourses <freshitpt@gmail.com>
2022-09-17 11:57:19 +0300

    bcrypt sample

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const http = require('http');
+const bcrypt = require('bcrypt');
 const requestListener = require('./requestListener');
 const PORT = 3000;
+
+const password = 'admin';
+const passwordHash = bcrypt.hashSync(password, 10);
+console.log('password hash = ', passwordHash);
+
 
 const server = http.createServer(requestListener);
 server.listen(PORT);
