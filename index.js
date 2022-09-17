e1c3e0e085f106ea0d226206a76d693d7c88e864
pdcourses <freshitpt@gmail.com>
2022-09-17 12:12:48 +0300

    send http from server

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const http = require('http');
-const bcrypt = require('bcrypt');
-const requestListener = require('./requestListener');
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+const HOST = '127.0.0.1'; // 'localhost'
 
-const password = 'admin';
-const passwordHash = bcrypt.hashSync(password, 10);
-console.log('password hash = ', passwordHash);
+const server = http.createServer( (req, res) => {
+    const {url, method} = req;
+    console.log('url', url);
+    console.log('method', method);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8"></meta>
+        <title>Simple page</title>
+    </head>
+    <body>
+    <h1>Home page</h1>
+    <p>lorem</p>
+    </body>
+    </html>
+    `);
+});
 
-
-const server = http.createServer(requestListener);
-server.listen(PORT);
+server.listen(PORT, HOST, () => {
+    console.log(`Server is listening ${HOST} on ${PORT} port!`);
+});
 
 
