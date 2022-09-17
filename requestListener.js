90ba0312ba541e4ac295422161c9335e85e03cf8
pdcourses <freshitpt@gmail.com>
2022-09-17 12:50:02 +0300

    fix requestListener restructure

--- requestListener.js
+++ requestListener.js
@@ -6,28 +6,32 @@ const readFile = util.promisify(fs.readFile);
 
 const requestListener = (req, res) => {
   const { url, method } = req;
-  const page = url == "/" ? "index.html" : url;
-  const regPage = /^.*\.html$/;
-  if (regPage.test(page)) {
-    const pagePath = path.join(__dirname, "/pages/", page);
-    if (fs.existsSync(pagePath, { encoding: "utf-8" })) {
-      readFile(pagePath)
-        .then((data) => {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "text/html");
-          res.end(data);
-        })
-        .catch((err) => {
-          res.statusCode = 500;
-          res.end("Server error");
-        });
-    } else {
-      res.statusCode = 404;
-      res.end("Page not found");
-    }
-  }
+
   switch (method) {
     case "GET": {
+      const page = url == "/" ? "index.html" : url;
+      const regPage = /^.*\.html$/;
+      if (regPage.test(page)) {
+        const pagePath = path.join(__dirname, "/pages/", page);
+        if (fs.existsSync(pagePath, { encoding: "utf-8" })) {
+          readFile(pagePath)
+            .then((data) => {
+              res.statusCode = 200;
+              res.setHeader("Content-Type", "text/html");
+              res.end(data);
+            })
+            .catch((err) => {
+              res.statusCode = 500;
+              res.end("Server error");
+            });
+        } else {
+          res.statusCode = 404;
+          res.end("Page not found");
+        }
+      } else{
+        res.statusCode = 403;
+        res.end("Resource is not html");
+      }
       break;
     }
     default: {
