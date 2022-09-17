89cd3506230ba83c2206e36c85ee54fae8532fb3
pdcourses <freshitpt@gmail.com>
2022-09-17 12:39:02 +0300

    request listener: pages sending

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 const HOST = '127.0.0.1'; // 'localhost'
+const requestListener = require('./requestListener');
 
-const server = http.createServer( (req, res) => {
-    const {url, method} = req;
-    console.log('url', url);
-    console.log('method', method);
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(`<!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8"></meta>
-        <title>Simple page</title>
-    </head>
-    <body>
-    <h1>Home page</h1>
-    <p>lorem</p>
-    </body>
-    </html>
-    `);
-});
+const server = http.createServer(requestListener);
 
 server.listen(PORT, HOST, () => {
     console.log(`Server is listening ${HOST} on ${PORT} port!`);
--- requestListener.js
+++ requestListener.js
@@ -1,5 +1,33 @@
-// req - request/ запрос, res - response/ ответ
-module.exports = function requestListener(req, res){
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+const requestListener = (req, res) => {
     const {url, method} = req;
-    res.end(`Hello! Your method is ${method} in url ${url}`); 
+    const page = url == '/' ? 'index.html' : url;
+    const regPage = /^.*\.html$/;
+    if(regPage.test(page)){
+        const pagePath = path.join(__dirname, '/pages/', page);
+        readFile(pagePath).then(data => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(data);
+        }).catch(err => {
+            res.statusCode = 500;
+            res.end('Server error');
+        })
+    }
+    switch(method){
+        case 'GET': { 
+            break;
+        }
+        default: {
+            res.statusCode = 400;
+            res.end('Bad request ((( ');
+        }
+    }
 }
+
+module.exports = requestListener;
